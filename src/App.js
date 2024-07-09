f9128299ec4f7bbfbc4d9f1057cc890fe8f75418
Wasid786 <wasidansari5284@gmail.com>
2024-07-09 11:46:27 +0530

    header component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import logo from "./logo.svg";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="bg-blue-500 text-3xl text-blue-950"> app js </div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Homr</div>;
+};
+
+export default Home;
